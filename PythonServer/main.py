031238eb6eef0a967984ee737d56efadf7935e21
Ankur Raj <67510454+ankur555raj@users.noreply.github.com>
2021-05-08 16:38:25 +0530

    angular Templates

--- PythonServer/main.py
+++ PythonServer/main.py
@@ -1,2 +1,33 @@
+from flask import Flask, jsonify;
+from flask_cors import CORS;
+app = Flask(__name__)
+CORS(app)
 
-print('Hello, python project!')
+weather = {
+     "data": [
+     {
+         "day": "1/6/2019",
+         "temperature": "23",
+         "windspeed": "16",
+         "event": "Sunny"
+     },
+     .
+     .
+     ]
+    }
+
+@app.route("/", methods=['GET'])
+def index():
+    return "Welcome to MyProject"
+
+def WeatherReport():
+     global weather
+     return jsonify([weather])
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
